7250754021f3a975c11fb1c28b569972fc3f6600
yukihirop <te108186@gmail.com>
2018-08-04 20:18:36 +0900

    Fix PR #3

--- lib/match_hash/sub/match_hash.rb
+++ lib/match_hash/sub/match_hash.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 module Sub
 
   private
 
-  def match_hash(expected, actual)
+  def match_hash?(expected, actual)
     expected.each do |key, value|
       return true unless actual.has_key?(key)
       @is_errors << match_value(expected[key], actual[key])
@@ -17,19 +19,29 @@ module Sub
     end
     @is_errors.include?(true)
   end
+  alias :match_hash :match_hash?
 
-  def match_array(expected, actual)
-    expected.each_with_index do |_,i|
-      match_value(expected[i],actual[i])
+  def match_array?(expected, actual)
+    expected.each_with_index do |data,i|
+      case data
+      when Hash
+        match_hash(data, actual[i])
+      when Array
+        match_array(data, actual[i])
+      else
+        match_value(data, actual[i])
+      end
     end
   end
+  alias :match_array :match_array?
 
   # trueを返すときがエラー
-  def match_value(expected, actual)
+  def match_value?(expected, actual)
     match_hash(expected, actual) if expected.is_a?(Hash)
     match_array(expected, actual) if expected.is_a?(Array)
     return false if expected == actual
     return true
   end
+  alias :match_value :match_value?
 
 end
--- spec/match_hash_spec.rb
+++ spec/match_hash_spec.rb
@@ -1,32 +1,33 @@
 require "spec_helper"
 
 describe MatchHash do
-
-  before do
-    @A = {
-        a11:11,
-        a12:12,
-        a13:13,
-        a14:{
-            b11:11,
-            b12:12
-        },
-        a15:[1,2,3],
-        a16:16,
-        a17: [{b13: 13}]
+  let(:expected) do
+    {
+      a11:11,
+      a12:12,
+      a13:13,
+      a14:{
+          b11:11,
+          b12:12
+      },
+      a15:[1,2,3],
+      a16:16,
+      a17: [{b13: 13}]
     }
+  end
 
-    @B = {
-        a11:11,
-        a12:12,
-        a13:13,
-        a14:{
-            b11:11,
-            b12:12
-        },
-        a15:[3,2,1],
-        a16:16,
-        a17: [{b13: 13}]
+  let(:actual) do
+    {
+      a11:11,
+      a12:12,
+      a13:13,
+      a14:{
+          b11:11,
+          b12:12
+      },
+      a15:[3,2,1],
+      a16:16,
+      a17: [{b13: 14}]
     }
   end
 
@@ -34,14 +35,27 @@ describe MatchHash do
     expect(MatchHash::VERSION).not_to be nil
   end
 
-  it "A hash match A hash" do
-    expect(@A).to match_hash(@A)
+  it "expected hash match expected hash" do
+    expect(expected).to match_hash(expected)
   end
 
-  it "A hash do not match B hash and occured raise_error" do
+  it "expected hash do not match actual hash and occured raise_error" do
+    # Error message
+    #
+    # RSpec::Expectations::ExpectationNotMetError:
+    # Diff:
+    #
+    # @@ -2,7 +2,7 @@
+    #  :a12 => 12,
+    #  :a13 => 13,
+    #  :a14 => {:b11=>11, :b12=>12},
+    # -:a15 => [3, 2, 1],
+    # +:a15 => [1, 2, 3],
+    #  :a16 => 16,
+    # -:a17 => [{:b13=>14}],
+    # +:a17 => [{:b13=>13}],
     expect do
-      expect(@A).to match_hash(@B)
+      expect(expected).to match_hash(actual)
     end.to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
-
 end
